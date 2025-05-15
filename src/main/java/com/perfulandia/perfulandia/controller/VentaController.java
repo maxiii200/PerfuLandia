297569824da900da3624adddb07f58b0a6f2d89d
Melipilla <melipilla@duoc.acad>
2025-05-15 17:59:28 -0400

    casi terminado

---
+++ src/main/java/com/perfulandia/perfulandia/controller/VentaController.java
@@ -0,0 +1,16 @@
+package com.perfulandia.perfulandia.controller;
+
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/v1/ventas")
+public class VentaController {
+
+    private final VentaService ventaService;
+
+    public VentaController(VentaService)
+}
